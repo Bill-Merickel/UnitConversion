b8fbd52102e0ef421fad4c3c51c737bdd53c2301
Bill-Merickel <billmerickel@gmail.com>
2025-03-22 17:16:36 -0400

    Initial Commit

---
+++ UnitConversion/UnitConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionApp.swift
+//  UnitConversion
+//
+//  Created by Bill Merickel on 3/22/25.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
